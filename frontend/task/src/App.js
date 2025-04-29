19b3727703d2d20fd41faf080531a02c7718a244
Santhosh4-7 <santhosh20047@gmail.com>
2025-04-29 19:29:50 +0530

    Update App.js

--- frontend/task/src/App.js
+++ frontend/task/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Register from './register'; // import your Register page
+import Register from './register'; 
 import Login from './login';
 import Home from './home'
 function App() {
