c9c8a17ddc8f1695c825146a6a8f6bdb83e7ed0c
Santhosh4-7 <santhosh20047@gmail.com>
2025-04-29 19:30:47 +0530

    Update register.js

--- frontend/task/src/register.js
+++ frontend/task/src/register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './register.css'; // Import the CSS for styling
+import './register.css'; 
 
 function Register() {
   const [email, setEmail] = useState('');
